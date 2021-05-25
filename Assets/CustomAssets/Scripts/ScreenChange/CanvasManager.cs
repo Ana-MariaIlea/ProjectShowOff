47beda04075886599c427a9237c15cff9952be35
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-05-25 11:29:26 +0200

    Screen change
    Easier change between screens

---
+++ Assets/CustomAssets/Scripts/ScreenChange/CanvasManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEditor;
+using System.Text;
+using TMPro;
+
+public enum CanvasType
+{
+    MainMenu,
+    Leaderboard,
+    Settings,
+    Credits
+}
+
+
+public class CanvasManager : MonoBehaviour
+{
+    public static CanvasManager instance;
+    List<CanvasController> canvasControllerList;
+    CanvasController lastActiveCanvas;
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+
+        canvasControllerList = GetComponentsInChildren<CanvasController>().ToList();
+        foreach (CanvasController item in canvasControllerList)
+        {
+            item.gameObject.SetActive(false);
+        }
+        SwitchCanvas(CanvasType.MainMenu);
+    }
+
+    public void SwitchCanvas(CanvasType _type)
+    {
+
+        if (lastActiveCanvas != null)
+        {
+            lastActiveCanvas.gameObject.SetActive(false);
+        }
+        CanvasController desiredScreen = canvasControllerList.Find(x => x.type == _type);
+        if (desiredScreen != null)
+        {
+            desiredScreen.gameObject.SetActive(true);
+            lastActiveCanvas = desiredScreen;
+        }
+    }
+
+    public CanvasController GetCurrentCanvas()
+    {
+        return lastActiveCanvas;
+    }
+}
