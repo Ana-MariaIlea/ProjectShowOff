c739e7ca899972544f13cc75f0999491cccaf38f
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-05-25 14:15:36 +0200

    Pause menu

---
+++ Assets/CustomAssets/Scripts/CoreSysyemScripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool IsGamePaused = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            if (IsGamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = 1f;
+        IsGamePaused = false;
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        IsGamePaused = true;
+    }
+}
