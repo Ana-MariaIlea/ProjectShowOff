0f303d20b16f6117af51a1c574dc77c6b3644802
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-06-08 14:25:25 +0200

    Localisation system added

--- Assets/CustomAssets/Scripts/Localization/LocalisationSystem.cs
+++ Assets/CustomAssets/Scripts/Localization/LocalisationSystem.cs
@@ -4,5 +4,48 @@ using UnityEngine;
 
 public class LocalisationSystem
 {
-  
+    public enum Language
+    {
+        English,
+        Dutch
+    }
+
+    public static Language language = Language.English;
+
+    private static Dictionary<string, string> localisedEN;
+    private static Dictionary<string, string> localisedNL;
+
+    public static bool isInit;
+
+    public static void Init()
+    {
+        CSVLoader csvLoader = new CSVLoader();
+        csvLoader.LoadCSV();
+
+        localisedEN = csvLoader.GetDictionaryValues("en");
+        localisedNL = csvLoader.GetDictionaryValues("nl");
+
+        isInit = true;
+    }
+
+    public static string GetLocalisedValue(string key)
+    {
+        if (!isInit)
+        {
+            Init();
+        }
+
+        string value = key;
+        switch (language)
+        {
+            case Language.English:
+                localisedEN.TryGetValue(key, out value);
+                break;
+            case Language.Dutch:
+                localisedNL.TryGetValue(key, out value);
+                break;
+        }
+
+        return value;
+    }
 }
