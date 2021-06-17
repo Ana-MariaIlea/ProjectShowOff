585067feec15e5e0eb10374714a1b2fa342b8732
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-06-17 14:42:59 +0200

    Added sounds to the main menu

--- Assets/CustomAssets/Scripts/TestFlower.cs
+++ Assets/CustomAssets/Scripts/TestFlower.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class TestFlower : MonoBehaviour
 {
+    public Material TestMaterial;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +24,21 @@ public class TestFlower : MonoBehaviour
             }
         }
         
+    }
+
+    
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "Pesticide")
+        {
+            // EventQueue.eventQueue.AddEvent(new PlayerPesticideCollisionEventData());
+            GetComponent<Renderer>().material = TestMaterial;
+            List<NectarDistributor> distributors = GetComponentsInChildren<NectarDistributor>().ToList();
+            for (int i = distributors.Count-1; i > 0; i--)
+            {
+                NectarDistributor d = distributors[i];
+
+            }
+        }
     }
 }
