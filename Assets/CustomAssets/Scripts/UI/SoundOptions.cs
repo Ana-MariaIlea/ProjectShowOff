2c298a7d9368314a9f2941459af2c9778a101e6f
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-06-21 20:52:24 +0200

    Menu adjust sound

--- Assets/CustomAssets/Scripts/UI/SoundOptions.cs
+++ Assets/CustomAssets/Scripts/UI/SoundOptions.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class SoundOptions : MonoBehaviour
 {
-    public string nameTestSoundSFX;
+   // public string nameTestSoundSFX;
     FMOD.Studio.Bus Music;
     FMOD.Studio.Bus Sound;
+    [FMODUnity.EventRef]
+    public string fmodSoundEvent;
     FMOD.Studio.EventInstance SFXTestSound;
 
     private void Awake()
     {
         Music = FMODUnity.RuntimeManager.GetBus("bus:/Master/Background music"); 
         Sound = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
-        SFXTestSound = FMODUnity.RuntimeManager.CreateInstance("event:/" + nameTestSoundSFX);
+        SFXTestSound = FMODUnity.RuntimeManager.CreateInstance(fmodSoundEvent);
         Debug.Log(Music);
         Debug.Log(Sound);
     }
