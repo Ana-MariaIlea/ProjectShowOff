ed278675fb04fda7370a211b1b250e19613c2692
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-06-22 13:01:59 +0200

    Bee sounds in code implemented

---
+++ Assets/CustomAssets/Scripts/EventQueue/Events/Sound/PlayBeeTakeOffSoundEventData.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayBeeTakeOffSoundEventData : EventData
+{
+    public PlayBeeTakeOffSoundEventData() : base(EventType.PLAYBEETAKEOFFSOUND) { }
+}
