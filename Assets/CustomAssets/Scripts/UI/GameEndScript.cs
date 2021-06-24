b4f877cc985f6cf64e11afda9f5518de3c4150cd
Ana-Maria Ilea <62722821+Ana-MariaIlea@users.noreply.github.com>
2021-06-24 09:44:58 +0200

    Gameend scripts

---
+++ Assets/CustomAssets/Scripts/UI/GameEndScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class GameEndScript : MonoBehaviour
+{
+    [SerializeField]
+    VideoClip englishCutscene;
+    [SerializeField]
+    VideoClip dutchCutscene;
+    VideoPlayer player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<VideoPlayer>();
+        switch (LocalisationSystem.language)
+        {
+            case LocalisationSystem.Language.English:
+                player.clip = englishCutscene;
+                break;
+            case LocalisationSystem.Language.Dutch:
+                player.clip = dutchCutscene;
+                break;
+        }
+    }
+
+    public void PlayCutscene()
+    {
+        player.Play();
+    }
+}
